f107cb41afa17f89d753c0863f8829fa061ee6d9
dranelol <cardsfan853@gmail.com>
2015-02-05 22:53:09 -0600

    added title card, changed gui to reflect

---
+++ Assets/Scripts/FadeWidget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeWidget : MonoBehaviour 
+{
+    public GameObject ComponentToFade;
+    TweenAlpha tween;
+
+    void Awake()
+    {
+        tween = ComponentToFade.GetComponent<TweenAlpha>();
+    }
+
+    public void FadeOut()
+    {
+        tween.enabled = true;
+        tween.PlayForward();
+    }
+
+    public void FadeIn()
+    {
+        tween.enabled = true;
+        tween.PlayReverse();
+    }
+}
