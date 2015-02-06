1b2fce33b33fa82e704ebdd24f77609558a6a17c
dranelol <cardsfan853@gmail.com>
2015-02-06 00:08:20 -0600

    game info gui working, need to edit in values

---
+++ Assets/Scripts/GameInfo.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInfo : MonoBehaviour 
+{
+    public bool Activated;
+
+    TweenScale tweenScale;
+    TweenAlpha tweenAlpha;
+
+    void Awake()
+    {
+        tweenScale = GetComponent<TweenScale>();
+        tweenAlpha = GetComponent<TweenAlpha>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GameInfoClicked()
+    {
+        if (Activated == true)
+        {
+            Deactivate();
+        }
+
+        else
+        {
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+        Activated = true;
+
+        tweenScale.PlayForward();
+        tweenAlpha.PlayForward();
+    }
+
+    public void Deactivate()
+    {
+        Activated = false;
+
+        tweenScale.PlayReverse();
+        tweenAlpha.PlayReverse();
+    }
+}
